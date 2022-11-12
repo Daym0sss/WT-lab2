4974c7c0347de4b873a0ddb19f650c6796a4cdd6
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-11-12 17:31:29 +0300

    Create DAOFactory.java

---
+++ src/main/task1/dao/DAOFactory.java
@@ -0,0 +1,22 @@
+package task1.dao;
+
+import task1.dao.impl.FileApplianceDAOImpl;
+
+/**
+ *
+ */
+public final class DAOFactory {
+	private static final DAOFactory instance = new DAOFactory();
+
+	private final ApplianceDAO applianceDAO = new FileApplianceDAOImpl();
+	
+	private DAOFactory() {}
+
+	public ApplianceDAO getApplianceDAO() {
+		return applianceDAO;
+	}
+
+	public static DAOFactory getInstance() {
+		return instance;
+	}
+}
