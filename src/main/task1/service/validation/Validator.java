d8f53121e9074f041d508fd9be49fcc072c873b6
Daym0sss <101497821+Daym0sss@users.noreply.github.com>
2022-11-12 17:41:30 +0300

    Create Validator.java

---
+++ src/main/task1/service/validation/Validator.java
@@ -0,0 +1,11 @@
+package task1.service.validation;
+
+import task1.entity.criteria.Criteria;
+
+public class Validator {
+
+    public static boolean criteriaValidator(Criteria criteria) {
+        return true;
+    }
+
+}
